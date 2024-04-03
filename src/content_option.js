bca3bd84fa1f9b7c194e2b063f09d72633d9d2a5
Irwin Gonzales <irwingonzales@ymail.com>
2024-04-02 20:34:04 -0700

    finished website

--- src/content_option.js
+++ src/content_option.js
@@ -1,156 +1,143 @@
-const logotext = "JOHN";
+const logotext = "IRWIN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Irwin Gonzales",
+    description: "I’m Irwin Gonzales iOS engineer and machine learning student, currently working in the Bay Area",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Irwin Gonzales",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Software engineer by day, competitive boxer by night",
+        second: "I code iOS Apps (among many other things)",
+        third: "Current main quest: Machine Learning",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "I want to build things that make this world a better place",
+    your_img_url: "https://storage.cloud.google.com/irwinportfoliobucket/IMG_2947.JPG",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "A bit about my self",
+    aboutme: "Self taught iOS developer that started in 2015 that learned how to build a bunch of stuff like websites, games, AR platforms and now AI apps. My mission is to build cool technology that makes the world a better place, whatever that may be. Currently a student in Machine Learning and on the road to being an ML Engineer.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Principal iOS Engineer",
+        where: "Third Summit",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Senior iOS Engineer",
+        where: "Jackpocket",
+        date: "2022",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Senior Software Engineer",
+        where: "FORME",
+        date: "2021",
     },
 ];
 
 const skills = [{
-        name: "Python",
+        name: "Swift",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "SwiftUI",
         value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
     },
     {
         name: "React",
         value: 60,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "Machine Learning",
+        value: 40,
+    },
+    {
+        name: "Python",
+        value: 75,
     },
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "iOS, iPad & MacOS Apps",
+        description: "Building iOS, MacOS and iPadOS apps is my bag, since 2015 and Objective-C. I've used Swift and UIKit but as of 2020, I've transitioned to SwiftUI. I've either contributed or built iOS apps of all sizes (MVP, internal tools and enterprise apps) that are live in the App Store.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "AR/VR & Web Technologies",
+        description: "Though I would say AR/VR & Web Technologies were my side quests, I learned a lot and apply persective to any project I'm on. I've worked on cool AR apps that were displayed in Verizon stores and VR scanners that were used in NCAA March Madness tournaments and NBA All-Star Weekend. I've worked on anything between Mesh network systems to SAAS enterprise software.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Machine Learning & Computer Vision",
+        description: "With my new main quest in Machine Learning I've worked on a ton of technologies involving generative models, training models with proper datasets, computer vision and app store ready AI applications.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/IMG_2341.jpg",
+        description: "Alteon.io",
+        link: "https://apps.apple.com/us/app/alteon-io/id1666739505",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/Screenshot%202024-04-02%20at%207.04.22%E2%80%AFPM.png",
+        description: "XR Avatar Scanner",
+        link: "https://groovejones.com/scanme",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/IMG_2344.jpg",
+        description: "Mejier",
+        link: "https://www.meijer.com/",
+    },
+    
+
+    {
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/IMG_2342.jpg",
+        description: "Forme Life",
+        link: "https://formelife.com/",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/IMG_2345.jpg",
+        description: "YikYak",
+        link: "https://apps.apple.com/us/app/yik-yak/id1573043816",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/IMG_2343.jpg",
+        description: "Jackpocket",
+        link: "https://apps.apple.com/us/app/jackpocket-lottery-app/id663046683",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/IMG_2350.jpg",
+        description: "Tealeaf",
+        link: "https://www.acoustic.com/",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/IMG_2348.jpg",
+        description: "Achilles AI",
+        link: "https://www.tryachilles.com/",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://storage.cloud.google.com/irwinportfoliobucket/Screenshot%202024-04-02%20at%207.10.26%E2%80%AFPM.png",
+        description: "Jinx VR Experience",
+        link: "https://groovejones.com/verizon-5g-and-riot-game-league-of-legends-5g-mec-mobile-virtual-production/",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "irwindev92@gmail.com",
+    // YOUR_FONE: "(650)580-3173",
+    description: "I'm excited to hear from you! Don't be afraid to shoot me any questions, comments or concerns. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: "service_iwvx7x8",
+    YOUR_TEMPLATE_ID: "template_iahh8dh",
+    YOUR_USER_ID: "AsXtG-0iX1hOaECS_",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/irwingonzales92",
+    // facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/irwin-gonzales-88523a59/",
+    twitter: "https://twitter.com/IrwinGonzales92",
 };
 export {
     meta,
